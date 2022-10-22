a3ce5ef50d197ca33fa9db3a7c2cbc9286c50f18
Mikel <mikelalda13@gmail.com>
2022-10-22 12:17:47 +0200

    added robaka-ros submodule

--- hoverboard_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikel/hoverboard_ws/src
-# Build directory: /home/mikel/hoverboard_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("robaka")
--- hoverboard_ws/build/robaka/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikel/hoverboard_ws/src/robaka
-# Build directory: /home/mikel/hoverboard_ws/build/robaka
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
